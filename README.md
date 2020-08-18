46e37fa6dbc3d292f09d0ebf6948437df81fe964
coderius <sunrise4fun@gmail.com>
2020-08-18 22:55:51 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,142 @@
+# Yii2 upload file behavior #
+
+## About
+Yii2 upload file behavior - simple wey to upload images and files to server. 
+No need anymore wrote tonn of code in controller and else testing it by houers. As a result - saving time and labor costs for uploading files to the site.
+Only needed upload extention from github and past some less code to model class (\yii\db\ActiveRecord) where needed hendler uploading files.
+More on this below.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+First download extention. Run the command in the terminal:
+```
+composer require "coderius/yii2-upload-file-behavior"
+```
+
+or add in composer.json
+```
+"coderius/yii2-upload-file-behavior": "^1.0"
+```
+and run `composer update`
+
+## Usage
+This extention created for usage in \yii\db\ActiveRecord model classes.
+
+Configyration behavior.
+
+* __$nameOfAttributeFile__ = (string) default name 'file'. Virtual attribute for uploading file instance from file systrem.
+* __$nameOfAttributeStorage__ = (string) default name 'face_img'. Attribute for saving path to uploaded file in db.
+* __$newFileName__ = (string) name which is assigned to uploaded file
+* __$directories__ = (array) configs to upload folder and upload hendlers.Сonsists of separate arrays.
+    Each array contains settings for the path to the target folder and a handler for uploading files to this folder like 'path' and 'handler'
+        __-'path'__ - contains path to target folder
+        __-'hendler'__ - Processes the downloaded file and saves to the specified in param 'path' location.
+
+This extention created for usage in \yii\db\ActiveRecord model classes.
+
+- So, first in model class put namespace to yii2-upload-file-behavior. 
+- Create public variable $file for loading file from filesystem.
+- The database must have an attribute to store the file path. In example below it is 'img_src' attribute (marked like save in public function rules())
+- Then past needed configs behaviors() method like in example.
+
+__!Note.__ _Don't forget to include the dependency namespaces._
+
+**Example**
+
+```
+    namespase your/models;
+
+    use coderius\yii2UploadFileBehavior\UploadFileBehavior;
+    use yii\imagine\Image;
+    use Imagine\Image\Point;
+    use Imagine\Image\Box;
+
+    class YourModel extends \yii\db\ActiveRecord
+    {
+        public $file;
+
+        //'img_src' - attribute to save path to file in db
+        public function rules()
+        {
+            return [
+                [['img_src'], 'safe'],
+        }
+
+        ...
+
+            public function behaviors()
+            {
+                return [
+                    //Another behaviors
+                    //...
+
+                    'uploadFileBehavior' => [
+                        'class' => UploadFileBehavior::className(),
+                        'nameOfAttributeStorage' => 'img_src',
+                        'directories' => [
+                            
+                            [
+                                'path' => function($attributes){
+                                    return \Yii::getAlias('@portfoleoPhotosPath/' . $attributes['id'] . '/big/');
+                                },
+                                'hendler' => function($fileTempName, $newFilePath){
+                                    Image::thumbnail($fileTempName, 900, 900*2/3)
+                                    ->copy()
+                                    ->crop(new Point(0, 0), new Box(900, 900*2/3))
+                                    ->save($newFilePath, ['quality' => 80]);
+                                    sleep(1);
+                                }
+                            ],
+                            [
+                                'path' => function($attributes){
+                                    return \Yii::getAlias('@portfoleoPhotosPath/' . $attributes['id'] . '/middle/');
+                                },
+                                'hendler' => function($fileTempName, $newFilePath){
+                                    Image::thumbnail($fileTempName, 400, 400*2/3)
+                                    ->save($newFilePath, ['quality' => 80]);
+                                    sleep(1);
+                                }
+                            ],
+                            [
+                                'path' => function($attributes){
+                                    return \Yii::getAlias('@portfoleoPhotosPath/' . $attributes['id'] . '/small/');
+                                },
+                                'hendler' => function($fileTempName, $newFilePath){
+                                    Image::thumbnail($fileTempName, 150, 150*2/3)
+                                    ->save($newFilePath, ['quality' => 80]);
+                                    sleep(1);
+                                }
+                            ],
+                        ]
+                    ],
+
+                ];
+            }
+
+        ...
+    }    
+
+```
+
+
+Additional actions:
+-------------------------------------
+1. Create aliases to target folders for saving uploaded files.
+2. Create target folders in 'frontend/web' dirrectory like example.
+3. Don't forget create vertual attribute. If it named like '$file', then no need set config to 'nameOfAttributeFile'(default = (string)'file').
+
+## Testing
+
+```bash
+$ ./vendor/bin/phpunit
+```
+
+## Credits
+
+- [Sergio Coderius](https://github.com/coderius)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/UploadFileBehavior.php
@@ -0,0 +1,295 @@
+<?php
+//UploadFileBehavior.php
+
+namespace coderius\yii2UploadFileBehavior\UploadFileBehavior;
+
+use Closure;
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+use yii\web\UploadedFile;
+use yii\helpers\FileHelper;
+use backend\models\blog\BlogArticles;
+use yii\base\InvalidCallException;
+
+
+
+/**
+ *  UploadFileBehavior class
+ *  UploadFileBehavior helped create hendler for uploading files and images to server simply.
+ *  To use UploadFileBehavior, insert the following code to your ActiveRecord class:
+ *
+ *  ```php
+ * 
+ *   namespase your/models;
+ *
+ *   use coderius\yii2UploadFileBehavior\UploadFileBehavior;
+ *   use yii\imagine\Image;
+ *    use Imagine\Image\Point;
+ *    use Imagine\Image\Box;
+ *
+ *    class YourModel extends \yii\db\ActiveRecord
+ *    {
+ *        public $file;
+ *
+ *        //'img_src' - attribute to save path to file in db
+ *        public function rules()
+ *        {
+ *            return [
+ *                [['img_src'], 'safe'],
+ *        }
+ *
+ *        ...
+ *
+ *            public function behaviors()
+ *            {
+ *                return [
+ *                    //Another behaviors
+ *                    //...
+ *
+ *                    'uploadFileBehavior' => [
+ *                        'class' => UploadFileBehavior::className(),
+ *                        'nameOfAttributeStorage' => 'img_src',
+ *                        'directories' => [
+ *                            
+ *                            [
+ *                                'path' => function($attributes){
+ *                                    return \Yii::getAlias('@portfoleoPhotosPath/' . $attributes['id'] . '/big/');
+ *                                },
+ *                                'hendler' => function($fileTempName, $newFilePath){
+ *                                    Image::thumbnail($fileTempName, 900, 900*2/3)
+ *                                    ->copy()
+ *                                    ->crop(new Point(0, 0), new Box(900, 900*2/3))
+ *                                    ->save($newFilePath, ['quality' => 80]);
+ *                                    sleep(1);
+ *                                }
+ *                            ],
+ *                            [
+ *                                'path' => function($attributes){
+ *                                    return \Yii::getAlias('@portfoleoPhotosPath/' . $attributes['id'] . '/middle/');
+ *                                },
+ *                                'hendler' => function($fileTempName, $newFilePath){
+ *                                    Image::thumbnail($fileTempName, 400, 400*2/3)
+ *                                    ->save($newFilePath, ['quality' => 80]);
+ *                                    sleep(1);
+ *                                }
+ *                            ],
+ *                            [
+ *                                'path' => function($attributes){
+ *                                    return \Yii::getAlias('@portfoleoPhotosPath/' . $attributes['id'] . '/small/');
+ *                                },
+ *                                'hendler' => function($fileTempName, $newFilePath){
+ *                                    Image::thumbnail($fileTempName, 150, 150*2/3)
+ *                                    ->save($newFilePath, ['quality' => 80]);
+ *                                    sleep(1);
+ *                                }
+ *                            ],
+ *                        ]
+ *                    ],
+ *
+ *                ];
+ *            }
+ *
+ *        ...
+ *    }    
+ *
+ *  ```
+ * 
+ * @author Sergio Coderius <sunrise4fun@gmail.com>
+ * @since 2.0
+ */
+class UploadFileBehavior extends Behavior
+{
+    /**
+     * Name of attribute for recording file from form to ActiveRecord. DEfault name of attribute is `file`
+     *
+     * @var [string]
+     */
+    public $nameOfAttributeFile = 'file';
+
+    /**
+     * @var string
+     */
+    public $nameOfAttributeStorage = 'face_img';
+    
+    /**
+     * Image name for renaming file and in next saving in file system and db
+     *
+     * @var [string]
+     */
+    public $newFileName;
+
+    /**
+     * Configuretion array for setting target paths to folders and uploading hendlers like in example:
+     *
+     * [
+     *    'path' => function($attributes){
+     *          return \Yii::getAlias('@portfoleoPhotosPath/' . $attributes['id'] . '/middle/');
+     *     },
+     *     'hendler' => function($fileTempName, $newFilePath){
+     *           Image::thumbnail($fileTempName, 400, 400*2/3)
+     *                 ->save($newFilePath, ['quality' => 80]);
+     *            sleep(1);
+     *     }
+     * ],
+     *
+     * @var [array]
+     */
+    public $directories;
+
+    /**
+     * File instance populated by yii\web\UploadedFile::getInstance
+     *
+     * @var null|UploadedFile the instance of the uploaded file.
+     */
+    private $fileInstance;
+
+    /**
+     * Undocumented variable
+     *
+     * @var [boolean | int]
+     */
+    private $time = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attach($owner)
+    {
+        parent::attach($owner);
+        $this->time = time();
+        $this->setFileInstance();
+        
+    }
+
+     /**
+     * {@inheritdoc}
+     */
+    public function events()
+    {
+        return [
+            BlogArticles::EVENT_BEFORE_VALIDATE => 'afterLoadHendler',
+            ActiveRecord::EVENT_AFTER_INSERT => 'afterInsertHendler',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'afterUpdateHendler',
+        ];
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $event
+     * @return void
+     */
+    public function afterLoadHendler($event)
+    {
+        if($this->isFile()){
+            $this->owner->file = $this->getFileInstance();//virtual attribute
+            $this->owner->{$this->nameOfAttributeStorage} = $this->getNewFileName();
+        }
+    }
+    
+    /**
+     * Undocumented function
+     *
+     * @param [type] $event
+     * @return void
+     */
+    public function afterInsertHendler($event)
+    {
+        $this->hendlersReducer(true);
+    }
+    
+    /**
+     * Undocumented function
+     *
+     * @param [type] $event
+     * @return void
+     */
+    public function afterUpdateHendler($event)
+    {
+        $this->hendlersReducer(false);
+    }          
+    
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $insert
+     * @return void
+     */
+    protected function hendlersReducer($insert)
+    {
+        if ($this->isFile())
+        {
+            foreach($this->directories as $dir){
+                if ($dir['path'] instanceof Closure || (is_array($dir['path']) && is_callable($dir['path']))) {
+                    $dirPath = $dir['path']($this->owner->attributes);
+                }else{
+                    throw new InvalidCallException('Param `path` mast be instanceof Closure or callable method.');
+                }
+                
+                if(!$insert){
+                    FileHelper::removeDirectory($dirPath);
+                }
+                
+                FileHelper::createDirectory($dirPath);
+                $newFilePath = $dirPath . $this->getNewFileName();
+
+                if ($dir['hendler'] instanceof Closure || (is_array($dir['hendler']) && is_callable($dir['hendler']))) {
+                    $dir['hendler']($this->getFileInstance()->tempName, $newFilePath);
+                }else{
+                    throw new InvalidCallException('Param `hendler` mast be instanceof Closure or callable method.');
+                }
+            }
+        }
+    }
+
+    /**
+     * Get the instance of the uploaded file.
+     *
+     * @return  null|UploadedFile
+     */ 
+    protected function getFileInstance()
+    {
+        return $this->fileInstance;
+    }
+
+    /**
+     * Set the instance of the uploaded file.
+     *
+     * @param  null|UploadedFile  $fileInstance  the instance of the uploaded file.
+     *
+     * @return  self
+     */ 
+    protected function setFileInstance()
+    {
+        $this->fileInstance = UploadedFile::getInstance($this->owner, $this->nameOfAttributeFile);
+    }
+
+    /**
+     * Get image name for renaming file and in next saving in file system and db
+     *
+     * @return  [string]
+     */ 
+    protected function getNewFileName()
+    {
+        $file = $this->getFileInstance();
+        $baseName = $file->baseName;
+        $ext = $file->extension;
+        
+         return $this->newFileName ?
+            $this->newFileName . '.' . $file->extension :
+            $file->baseName . '_' . $this->time . '.' . $file->extension;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return boolean
+     */
+    protected function isFile()
+    {
+        return  $this->getFileInstance() && $this->getFileInstance()->tempName;
+        
+    }
+
+}
